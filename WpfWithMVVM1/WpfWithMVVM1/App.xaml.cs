d7d30ee176691d9ad06717e0693915c7d0496c28
arushiv-optimus <arushi.vishwakarma@optimusinfo.com>
2017-10-30 17:08:33 +0530

    WPF Sample Peoject using MVVM

---
+++ WpfWithMVVM1/WpfWithMVVM1/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfWithMVVM1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            WpfWithMVVM1.MainWindow window = new MainWindow();
+            UserViewModel.UserViewModel VM = new UserViewModel.UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
