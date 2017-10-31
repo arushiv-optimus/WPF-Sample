59d5bd6a8abf74e7234c2645b00f8271d39adf65
arushiv-optimus <arushi.vishwakarma@optimusinfo.com>
2017-10-31 17:00:23 +0530

    Structure of the project modified as per MVVM

--- WpfWithMVVM1/WpfWithMVVM1/App.xaml.cs
+++ WpfWithMVVM1/WpfWithMVVM1/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 
 namespace WpfWithMVVM1
@@ -13,13 +8,6 @@ namespace WpfWithMVVM1
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            WpfWithMVVM1.MainWindow window = new MainWindow();
-            UserViewModel.UserViewModel VM = new UserViewModel.UserViewModel();
-            window.DataContext = VM;
-            window.Show();
-        }
+
     }
 }
--- WpfWithMVVM1/WpfWithMVVM1/MainWindow.xaml.cs
+++ WpfWithMVVM1/WpfWithMVVM1/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace WpfWithMVVM1
 {
@@ -22,6 +11,9 @@ namespace WpfWithMVVM1
     {
         public MainWindow()
         {
+            UserViewModels VM = new UserViewModels();
+            DataContext = VM;
+            Show();
             InitializeComponent();
         }
     }
