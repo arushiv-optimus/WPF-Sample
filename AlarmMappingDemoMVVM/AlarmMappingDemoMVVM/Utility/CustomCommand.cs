13db6e959e5145cf408feaffb53b9c93239e3739
arushiv-optimus <arushi.vishwakarma@optimusinfo.com>
2017-11-08 10:26:48 +0530

    Custom Command Added

---
+++ AlarmMappingDemoMVVM/AlarmMappingDemoMVVM/Utility/CustomCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AlarmMappingDemoMVVM.Utility
+{
+    /// <summary>
+    ///  CustomCommand class implemented ICommand Interface
+    /// </summary>
+    public class CustomCommand : ICommand
+    {
+        /// <summary>
+        /// Action is a generic delegates present in System namespace. it takes one or more input parameters and return Void.
+        /// </summary>
+        private Action<object> execute;
+
+        /// <summary>
+        /// predicates are the comparison delegates which take only one argument and retuen bool.
+        /// </summary>
+        private Predicate<object> canExecute;
+
+        /// <summary>
+        /// CustomCommand Constructor
+        /// </summary>
+        public CustomCommand(Action<object> execute, Predicate<object> canExecute)
+        {          
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        ///  check if canExecute is null return true else return canExecute with a parameter of predicate.
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            bool b = canExecute == null ? true : canExecute(parameter);
+            return b;
+        }
+
+        /// <summary>
+        /// Execute method will be called to execute somting on view model. Used to executing code basically.
+        /// </summary>
+        public void Execute(object parameter)
+        {
+            //Function or Method i want to execute
+            execute(parameter);
+        }
+    }
+}
